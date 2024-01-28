91e4779a3def1bb34ad669fd40b6eae657dbda90
Bruno Stefano de Souza <40743118+vonkoln@users.noreply.github.com>
2024-01-28 23:11:50 +0000

    ..

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-function updateTime() {
-  const timeElement = document.getElementById('time');
-  const now = new Date();
-  const hours = now.getHours().toString().padStart(2, '0');
-  const minutes = now.getMinutes().toString().padStart(2, '0');
-  const seconds = now.getSeconds().toString().padStart(2, '0');
-  const timeString = `${hours}:${minutes}:${seconds}`;
-  timeElement.textContent = timeString;
-}
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
 
-setInterval(updateTime, 1000); // Atualiza a cada segundo
-updateTime(); // Chama a função para exibir o tempo atual imediatamente
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+
+    if (min < 10) min = '0' + min;
+
+    if (s < 10) s = '0' + s;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
+
+})
